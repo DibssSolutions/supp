397e5e38e43ad338e06abb39998b3dd444a3b80e
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-09-17 23:29:17 +0300

    range functionality

--- src/js/components/_rangeSlider.js
+++ src/js/components/_rangeSlider.js
@@ -1,18 +1,51 @@
 var noUiSlider = require('nouislider');
 var wNumb = require('wnumb');
 
-function initRange(slider) {
-  // var minSlider = parseInt(slider.dataset['rangeMin']);
-  // var maxSlider = parseInt(slider.dataset['rangeMax']);
-  // var startSlider = parseInt(slider.dataset['rangeStart']);
-  // var endSlider = parseInt(slider.dataset['rangeEnd']);
+$(document).ready(function() {
 
+  var resSlider = document.querySelector('.js-result-slider');
+
+  if (resSlider) {
+    noUiSlider.create(resSlider, {
+      start: 25,
+      range: {
+        min: 7.6,
+        max: 180
+      },
+      tooltips: [wNumb({ decimals: 1 })]
+    });
+  }
+
+  var sliders = $('.js-range-slider');
+  if ($(sliders).length) {
+    $(sliders).each((i, slider) => initRange(slider));
+
+    $(sliders).each((i, slider) => {
+      var parent = $(slider).parents('.js-range-container');
+      var input = parent.find('.js-input');
+
+      slider.noUiSlider.on('update.one', function(e) {
+        input.val(e[0]);
+        var res = calcBMI();
+        resSlider.noUiSlider.set(res);
+      });
+
+      input.on('change', function() {
+        slider.noUiSlider.set(this.value);
+        var res = calcBMI();
+
+        resSlider.noUiSlider.set(res);
+      });
+    });
+  }
+});
+
+function initRange(slider) {
   var minSlider = parseInt($(slider).data('range-min'));
   var maxSlider = parseInt($(slider).data('range-max'));
 
   noUiSlider.create(slider, {
-    start: maxSlider / 2,
-
+    start: (maxSlider + minSlider) / 2,
     range: {
       min: minSlider,
       max: maxSlider
@@ -20,50 +53,11 @@ function initRange(slider) {
   });
 }
 
-var sliders = $('.js-range-slider');
-
-if ($(sliders).length) {
-  $(sliders).each((i, slider) => initRange(slider));
-
-  $(sliders).each((i, slider) => {
-    // var parent = slider.closest('.js-range-container');
-    var parent = $(slider).parents('.js-range-container');
-    var input = parent.find('.js-input');
-
-
-    slider.noUiSlider.on('update.one', function(e) {
-      input.val(e[0]);
-    });
-
-    // min.on('change', function() {
-    //   slider.noUiSlider.set([this.value, null]);
-    // });
-    // max.on('change', function() {
-    //   slider.noUiSlider.set([null, this.value]);
-    // });
-  });
-}
-
 function calcBMI() {
-  var e = $('#bmi-body-size-input').val(),
-    t = $('#bmi-weight-size-input').val(),
-    n = t / (e / 100 * (e / 100));
-  return n = n.toFixed(1), setTextBMI(n), n;
+  var e = $('.js-height-range').val(),
+    t = $('.js-weight-range').val(),
+    n = t / ((e / 100) * (e / 100));
+  return (n = n.toFixed(1));
 }
 
-var resSlider = document.querySelector('.js-result-slider');
-if (resSlider) {
-  var minSlider = parseInt($(resSlider).data('range-min'));
-  var maxSlider = parseInt($(resSlider).data('range-max'));
 
-
-  noUiSlider.create(resSlider, {
-    start: maxSlider / 2,
-
-    range: {
-      min: minSlider,
-      max: maxSlider
-    }, 
-    tooltips: [wNumb({decimals: 1})],
-  });
-}
